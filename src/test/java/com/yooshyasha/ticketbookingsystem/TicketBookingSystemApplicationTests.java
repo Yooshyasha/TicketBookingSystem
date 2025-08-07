e2f50e375726801638e24606ac5eea520140c452
Yooshyasha <mejzikrejzi66@gmail.com>
2025-08-07 18:53:49 +0500

    Init

---
+++ src/test/java/com/yooshyasha/ticketbookingsystem/TicketBookingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yooshyasha.ticketbookingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketBookingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
